d714b61ced04aeb908c80a899975cfe01fbcf609
Maxim Lamanovskiy <maxim.lamanovsky@7bits.it>
2019-10-02 10:48:34 +0600

    Using squash command to squash two last commits(Counter + Main): Made another counter
    Applying commit witch Counter.java fron third-branch
    
    Applying commit with Main from third-branch

--- src/Counter.java
+++ src/Counter.java
@@ -1,2 +1,7 @@
 public class Counter {
+    public void count(final int maxCount) {
+        for(int i = 0; i < maxCount; i++) {
+            System.out.println(i + 1);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!\n\n");
+    public static final int MAX_COUNT = 10;
 
+    public static void main(String[] args) {
+        final Counter counter = new Counter();
+        System.out.println("Hello, world!\n\n");
         System.out.println("Start counter");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i + 1);
+        
+        for(int i = 0; i < 5; i++) {
+            System.out.println(i);
         }
+
+        counter.count(MAX_COUNT);
         System.out.println("Stop counter");
     }
 }
