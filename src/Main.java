98aa6a2aabcfea859cb7182e705301f62799e374
Maxim Lamanovskiy <maxim.lamanovsky@7bits.it>
2019-10-02 10:48:34 +0600

    Made another counter

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!\n\n");
-
         System.out.println("Start counter");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i + 1);
+        
+        for(int i = 0; i < 5; i++) {
+            System.out.println(i);
         }
+
         System.out.println("Stop counter");
     }
 }
